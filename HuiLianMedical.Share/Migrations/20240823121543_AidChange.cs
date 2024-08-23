4f3d9651c231c1891b3480278360e5c6ee29f381
李嘉俊 <2383690354@qq.com>
2024-08-23 20:21:19 +0800

    更改Aid信息

---
+++ HuiLianMedical.Share/Migrations/20240823121543_AidChange.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HuiLianMedical.Share.Migrations
+{
+    /// <inheritdoc />
+    public partial class AidChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AidModel",
+                newName: "Num");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdCard",
+                table: "AidModel",
+                type: "varchar(18)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdCard",
+                table: "AidModel");
+
+            migrationBuilder.RenameColumn(
+                name: "Num",
+                table: "AidModel",
+                newName: "Name");
+        }
+    }
+}
